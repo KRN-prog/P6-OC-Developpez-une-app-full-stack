57e0fcd070ec21c07ff64dad598040c04292bc3b
Kyrian <kyrian.arvene@gmail.com>
2024-04-23 16:39:03 +0200

    Add security to the app

---
+++ back/src/main/java/com/openclassrooms/mdd/service/JWTService.java
@@ -0,0 +1,43 @@
+package com.openclassrooms.mdd.service;
+
+import java.time.Instant;
+import java.time.temporal.ChronoUnit;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.security.oauth2.jose.jws.MacAlgorithm;
+import org.springframework.security.oauth2.jwt.JwsHeader;
+import org.springframework.security.oauth2.jwt.JwtClaimsSet;
+import org.springframework.security.oauth2.jwt.JwtEncoder;
+import org.springframework.security.oauth2.jwt.JwtEncoderParameters;
+import org.springframework.stereotype.Service;
+
+import com.openclassrooms.mdd.usecase.dto.UserDto;
+import com.openclassrooms.mdd.usecase.dto.request.AuthRequestDto;
+
+@Service
+public class JWTService {
+    private JwtEncoder jwtEncoder;
+
+    public JWTService(JwtEncoder jwtEncoder) {
+        this.jwtEncoder = jwtEncoder;
+    }
+
+    public String genrerateToken(UserDto userDto, AuthRequestDto authRequestDto, Authentication authentication) {
+        String emailRequest;
+        if (userDto != null) {
+            emailRequest = userDto.getMail();
+        } else {
+            emailRequest = authRequestDto.getEmail();
+        }
+        Instant now = Instant.now();
+        JwtClaimsSet claims = JwtClaimsSet.builder()
+                .issuer("self")
+                .issuedAt(now)
+                .expiresAt(now.plus(1, ChronoUnit.HOURS))
+                .subject(emailRequest)
+                .build();
+        JwtEncoderParameters jwtEncoderParameters = JwtEncoderParameters
+                .from(JwsHeader.with(MacAlgorithm.HS256).build(), claims);
+        return this.jwtEncoder.encode(jwtEncoderParameters).getTokenValue();
+    }
+}
